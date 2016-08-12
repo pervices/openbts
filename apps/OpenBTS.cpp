6c074cb5c43f88c1cffe60be05728f7a6dc1634b
Namburi Raghuram <namburi.r@pervices.com>
2016-08-12 20:43:05 +0000

    Increasing transceiver turn ON wait time to 10s
    To ensure that transceiver is up before GSM starts

--- apps/OpenBTS.cpp
+++ apps/OpenBTS.cpp
@@ -622,7 +622,7 @@ int main(int argc, char *argv[])
 		transceiverThread.start((void*(*)(void*)) startTransceiver, NULL);
 		// sleep to let the FPGA code load
 		// TODO: we should be "pinging" the radio instead of sleeping
-		sleep(5);
+		sleep(10);
 	} else {
 		LOG(NOTICE) << "transceiver already running";
 	}
